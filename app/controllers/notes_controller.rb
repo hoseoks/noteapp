fe17c03862da93900f2683970a52d12f139ec112
Grace Nwankwo ☼ <gnwankwo@cps.edu>
2018-06-05 13:08:46 +0000

    Install postgres and update database.yml v

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,4 +3,13 @@ def index
 end
 def show
 end
+def new
+end
+def create
+t = Notes.new
+  t.description = params['notes']
+  t.pomodoro_estimate = params['unit']
+  t.save
+  redirect_to "/notes/show/#{ t.id }"
+end
 end
